ab705eedfa2e04e0f447b104bf32f53701ebf0af
Will <willpeng97@gmail.com>
2024-12-27 14:48:35 +0800

    .

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,6 +1,10 @@
-// import { Link } from 'react-router-dom';
-import './Navbar.css'
-import HomeIcon from '@/assets/icon-back-24.png';
+import './Navbar.css';
+import { Link } from 'react-router-dom';  // 如果使用 react-router-dom，將 href 改為 Link 元素
+import HomeIcon from '@/assets/icon-home-24.png';
+import BackIcon from '@/assets/icon-back-24.png';
+import SettingIcon from '@/assets/user-gear-solid.svg';
+import EarthIcon from '@/assets/earth-asia-solid.svg';
+import CartIcon from '@/assets/cart-shopping-solid.svg';
 
 export const Navbar = () => {
   return (
@@ -9,23 +13,25 @@ export const Navbar = () => {
         <div className="row w-100">
           <div className="col-5 d-flex align-items-center">
             <a className="navbar-brand" href="./index.html">
-              <h1 className="fs-4 m-0 p-0"><img src="./public/brand.png" alt="Brand of JINTEX" /></h1>
+              <h1 className="fs-4 m-0 p-0"><img src="/brand.png" alt="Brand of JINTEX" /></h1>
             </a>
-            <a href="./index.html" className="d-flex backButton">
-              <img src={HomeIcon} />
+
+            {/* 使用 Link 組件替換 href */}
+            <Link to="./index.html" className="d-flex backButton">
+              <img src={HomeIcon} alt="Home" />
               <p className="m-0">首頁</p>
-            </a>
-            
-            <a href="../index.html" className="d-flex backButton">
-              <img src="/assets/icon-back-24.png" />
+            </Link>
+
+            <Link to="../index.html" className="d-flex backButton">
+              <img src={BackIcon} alt="Back" />
               <p className="m-0">上一頁</p>
-            </a>
+            </Link>
           </div>
-    
+
           <div className="col-2 d-flex justify-content-center align-items-center">
             <h2 className="fs-4 m-0 p-0 text-light">主選單</h2>
           </div>
-    
+
           <div className="col-5 d-flex justify-content-end align-items-center">
             <div className="offcanvas offcanvas-end text-bg-dark" id="offcanvasNavbar2" aria-labelledby="offcanvasNavbar2Label">
               <div className="offcanvas-body">
@@ -35,21 +41,25 @@ export const Navbar = () => {
                       Welcome, <strong id="welcomeAccount"></strong>
                     </div>
                     <div className="inline welcome mx-2">
-                      <a href="" className="text-light"><img src="./img/comm/FontAwesome/earth-asia-solid.svg" alt="langue" /> 繁體中文</a>
+                      <a href="" className="text-light">
+                        <img src={EarthIcon} alt="Language" /> 繁體中文
+                      </a>
                     </div>
                     <div className="inline welcome mx-2">
-                      <a href="/JINTEX_EC/zz_query/cart.html" className="text-light" id="cartUrl"><img src="./img/comm/FontAwesome/cart-shopping-solid.svg" alt="cart" /> 購物車</a>
+                      <Link to="/JINTEX_EC/zz_query/cart.html" className="text-light" id="cartUrl">
+                        <img src={CartIcon} alt="Cart" /> 購物車
+                      </Link>
                     </div>
-                    <a className="inline nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      <img src="./assets/user-gear-solid.svg" alt="Admin option" />
-                    </a>
-                    {/* <ul className="dropdown-menu">
-                      <li><a className="dropdown-item" href="#" id="accountStatus">使用者</a></li>
-                      <li><a className="dropdown-item" href="#" id="changePassword" onclick="openChangePwdModal()">修改密碼</a></li>
-                      <li><a className="dropdown-item" href="#" id="lineMenber" onclick="openLineMenberModal()">Line會員</a></li>
-                      <li><a className="dropdown-item" href="#" id="contact" onclick="openContactModal()">聯絡我們</a></li>
-                      <li><a className="dropdown-item" href="#" id="logout">登出</a></li>
-                    </ul> */}
+                    <button className="inline nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                      <img src={SettingIcon} alt="Admin Options" />
+                    </button>
+                    <ul className="dropdown-menu">
+                      <li><Link to="#" className="dropdown-item" id="accountStatus">使用者</Link></li>
+                      <li><Link to="#" className="dropdown-item" id="changePassword" onClick={() => alert("修改密碼")}>修改密碼</Link></li>
+                      <li><Link to="#" className="dropdown-item" id="lineMenber" onClick={() => alert("Line會員")}>Line會員</Link></li>
+                      <li><Link to="#" className="dropdown-item" id="contact" onClick={() => alert("聯絡我們")}>聯絡我們</Link></li>
+                      <li><Link to="#" className="dropdown-item" id="logout">登出</Link></li>
+                    </ul>
                   </li>
                 </ul>
               </div>
@@ -58,8 +68,7 @@ export const Navbar = () => {
         </div>
       </div>
     </nav>
-  )
-  // return (
+  //    return (
   //   <nav style={{ backgroundColor: '#333', color: '#fff', padding: '10px' }}>
   //     <ul style={{ display: 'flex', listStyle: 'none', margin: 0, padding: 0 }}>
   //       <li style={{ margin: '0 10px' }}>
@@ -77,4 +86,5 @@ export const Navbar = () => {
   //     </ul>
   //   </nav>
   // );
+  );
 };
